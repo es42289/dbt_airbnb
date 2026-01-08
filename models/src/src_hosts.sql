bc5ae0d0abe5adf68a2d6c7e2fea8af7f9f962a2
es42289 <es42289@gmail.com>
2026-01-08 12:03:11 -0700

    starting at #33

---
+++ models/src/src_hosts.sql
@@ -0,0 +1,14 @@
+WITH raw_hosts AS (
+    SELECT
+        *
+    FROM
+        source('airbnb','hosts')
+)
+SELECT
+    created_at,
+    id AS host_id,
+    is_superhost,
+    name AS host_name,
+    updated_at
+FROM
+    raw_hosts
